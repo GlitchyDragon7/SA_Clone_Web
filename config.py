ec58c87d94e66f6e989d9b372d3bd27aad1245cc
GlitchyDragon7 <faaabilmusthafa@gmail.com>
2024-03-08 20:41:29 +0400

    FINISH

---
+++ config.py
@@ -0,0 +1,4 @@
+G_DRIVE_CLIENT_ID = "1019962325563-p17uvneek4dor3niadk7pqr157fhk282.apps.googleusercontent.com" 
+G_DRIVE_CLIENT_SECRET = "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"
+app_url = 'http://127.0.0.1:5200'
+service_acc_google_group_email = '' # optional
